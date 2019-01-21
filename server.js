882cda1be5ea6ee0204c7109bd49523c1655f1dc
R Sánchez <rene.as.himself@gmail.com>
2019-01-20 20:04:38 -0600

    Installed knex

--- server.js
+++ server.js
@@ -1,37 +1,65 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express')
+// const mongo = require('mongodb')
+// const mongoose = require('mongoose')
+const knex = require('knex')
 
-var cors = require('cors');
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-var app = express();
+const app = express()
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+// Basic Configuration
+const PORT = process.env.PORT || 3000
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
 
-app.use(cors());
+// Using Postgres
+const db = knex({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    }
+})
+
+app.use(cors())
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser())
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/views/index.html')
+})
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// your first API endpoint...
+app.get('/api/hello', (req, res) => {
+    res.json({ greeting: 'hello API' })
+})
 
+app.post('/api/shorturl/new', (req, res) => {
+    const body = req.body
+    console.log(body)
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
+    const originalURL = req.params
+    const domain = 'localhost:3000'
+    const url = 'XSLDK'
+
+    console.log(req.body)
+
+    const shortenedURL = {
+        original_url: `${originalURL}`,
+        short_url: `${domain}/${url}`,
+    }
+    res.json(shortenedURL)
+})
+
+// listen for requests :)
+const listener = app.listen(PORT, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+})
