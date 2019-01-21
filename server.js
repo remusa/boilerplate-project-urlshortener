7be96c92ae906d7b2e7667a1ce535d9eb11a6f09
R Sánchez <rene.as.himself@gmail.com>
2019-01-20 22:21:22 -0600

    Finished basic functionality

--- server.js
+++ server.js
@@ -20,9 +20,10 @@ const PORT = process.env.PORT || 3000
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        //   connectionString: process.env.DATABASE_URL,
+        connectionString: 'postgres://ctwspcxhwwuadk:a80262c817dcca8ffdf6d3e96eb64902e6c6ef4c98c8107bff6dc7dd73d070cd@ec2-107-20-183-142.compute-1.amazonaws.com:5432/df8m6odpsdag5g',
         ssl: true,
-    }
+    },
 })
 
 app.use(cors())
@@ -42,22 +43,73 @@ app.get('/api/hello', (req, res) => {
     res.json({ greeting: 'hello API' })
 })
 
-app.post('/api/shorturl/new', (req, res) => {
-    const body = req.body
-    console.log(body)
+app.get('/api/shorturl/:id', (req, res) => {
+    const { id } = req.params
 
-    const originalURL = req.params
-    const domain = 'localhost:3000'
-    const url = 'XSLDK'
+    db.select('*')
+        .from('urls')
+        .where('id', '=', id)
+        .then(result => {
+            const redirectURL = result[0].original_url
 
-    console.log(req.body)
-
-    const shortenedURL = {
-        original_url: `${originalURL}`,
-        short_url: `${domain}/${url}`,
-    }
-    res.json(shortenedURL)
+            console.log(`Redirecting to: ${redirectURL}`)
+            res.redirect(`${redirectURL}`)
+        })
+        .catch(err => {
+            console.log('Error retrieving url with that id')
+            res.status(400).json({
+                error: 'URL not found',
+            })
+        })
 })
+
+app.post('/api/shorturl/new', (req, res) => {
+    // Get the original url
+    const originalURL = req.body.url
+
+    // Get the current domain
+    const host = req.headers.host
+
+    // TODO: check invalid URL
+    if (!validateURL(originalURL)) {
+        return res.status(400).json({
+            error: 'invalid URL',
+        })
+    }
+
+    // Insert into database
+    insertURL(originalURL)
+
+    // Get the last 'id' inserted in the table 'urls'
+    db.select('id')
+        .from('urls')
+        .orderBy('id', 'DESC')
+        .limit(1)
+        .then(query => {
+            res.json({
+                original_url: `${originalURL}`,
+                short_url: `${host}/api/shorturl/${query[0].id}`,
+            })
+        })
+        .catch(err => console.log('Couldnt retrieve last inserted id'))
+})
+
+const validateURL = originalURL => {
+    const regex = /\w+\.\w+\.*\w*/
+
+    return originalURL.match(regex)
+}
+
+const insertURL = originalURL => {
+    db.transaction(trx => {
+        trx.insert({
+            original_url: originalURL,
+        })
+            .into('urls')
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
+}
 
 // listen for requests :)
 const listener = app.listen(PORT, () => {
